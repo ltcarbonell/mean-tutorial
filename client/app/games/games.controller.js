3a0215be0e4ddaa62f96dc7cd64a62a0cc321daa
Luis Carbonell <lt.carbonell@yahoo.com>
2016-05-22 08:44:10 -0400

    added /games

---
+++ client/app/games/games.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+(function(){
+
+class GamesComponent {
+  constructor() {
+    this.message = 'Hello';
+  }
+}
+
+angular.module('meanTutorialApp')
+  .component('games', {
+    templateUrl: 'app/games/games.html',
+    controller: GamesComponent
+  });
+
+})();
