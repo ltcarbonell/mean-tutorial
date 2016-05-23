2830ef7ef20cb3cff6950e3ca5114956e078c379
Luis Carbonell <lt.carbonell@yahoo.com>
2016-05-23 18:57:41 -0400

    allow users to delete and edit games

--- client/app/games/games.controller.js
+++ client/app/games/games.controller.js
@@ -1,44 +1,71 @@
 'use strict';
-
-(function() {
-
-  class GameController {
-
+(function(){
+  class GamesComponent {
     constructor($http) {
       this.$http = $http;
-    }
+      this.games = [];
+      this.filter = 'none';
+    };
 
     $onInit() {
       this.$http.get('/api/games').then(response => {
-          this.games = response.data;
-          console.log(this.games);
-        });
+        this.games = response.data;
+        this.originalGames = response.data;
+      });
+    };
 
-    }
-
-    addNewGame() {
-      console.log("added");
+    addGame() {
       if (this.newGame) {
-        this.$http.post('/api/games', {
-          name: this.newGame.name,
-          platform : this.newGame.platform,
-          genre: this.newGame.genre
-        });
+        this.$http.post('/api/games', { name: this.newGame.name , platform: this.newGame.platform, genre: this.newGame.genre});
         this.newGame = '';
         this.$http.get('/api/games').then(response => {
           this.games = response.data;
         });
       }
-    }
+    };
+    
+    resetGames(){
+      this.games = this.originalGames;
+      this.filter = 'none';
+    };
 
-    deleteGame(game) {
-      this.$http.delete('/api/games/' + game._id);
+    filterByGenre(genre){
+      this.resetGames();
+      this.filter = 'Genre: ' + genre;
+      this.games = this.games.filter(function(game){
+        return game.genre === genre;
+      });
+    };
+
+    filterByPlatForm(platform){
+      this.resetGames();
+      this.filter = 'Platform: ' + platform;
+      this.games = this.games.filter(function(game){
+        return game.platform === platform;
+      });
+    };
+
+    toggleEdit(index){
+      this.games[index].edit = !this.games[index].edit;
+    };
+
+    saveGame(index){
+      this.$http.put('/api/games/' + this.games[index]._id, this.games[index])
+      this.games[index].edit = false;
+    };
+
+    deleteGame(index) {
+      this.$http.delete('/api/games/' + this.games[index]._id);
+      this.$http.get('/api/games').then(response => {
+        this.games = response.data;
+      });
     }
   }
 
   angular.module('meanTutorialApp')
-    .component('games', {
-      templateUrl: 'app/games/games.html',
-      controller: GameController
-    });
+  .component('games', {
+    templateUrl: 'app/games/games.html',
+    controller: GamesComponent
+  });
+
 })();
